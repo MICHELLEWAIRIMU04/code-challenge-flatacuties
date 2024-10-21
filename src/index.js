f658c0c998aec13f0ccfcb7f4ed8e4fe5bef459d
MICHELLEWAIRIMU04 <michelle@michelle>
2024-10-21 14:44:01 +0300

    code-challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-// Your code here
+function fetchCharacters(){
+  fetch('http://localhost:3000/characters')
+  .then(response => response.json())
+  .then(characters => characters.forEach((character) => {
+     displayCharacterDetails(character)
+ }))
+ }
+fetchCharacters()
+function displayCharacterDetails(character){
+const characterBar = document.getElementById('character-bar')
+const span = document.createElement('span')
+span.textContent = character.name
+span.addEventListener('click', () => {
+  showDetails(character);
+});
+
+characterBar.appendChild(span)
+console.log(displayCharacterDetails)
+}
+function showDetails(character) {
+const detailedInfo = document.getElementById('detailed-info')
+document.getElementById('name').textContent = character.name
+document.getElementById('image').src = character.image
+document.getElementById('vote-count').textContent = `Votes: ${character.votes}`
+}
+
+
+const form = document.getElementById('votes-form')
+form.addEventListener ('submit', (e) => {
+  e.preventDefault()
+  const currentVotes = (document.getElementById('vote-count').textContent)
+  const additionalVotes = parseInt(document.getElementById('votes').value)
+  const newTotalVotes = currentVotes + additionalVotes
+  document.getElementById('vote-count').textContent = newTotalVotes
+  document.getElementById('votes').value = ''
+  })
+console.log(form)
+
